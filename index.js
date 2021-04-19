d88cb3e8d4365314722d46acd3b452327c2c45c9
eduardonot <eduardo_not@hotmail.com>
2021-04-19 10:19:00 -0300

    retirada de comentários

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 var loteria = require('./Loteria')
-// JOGO LOTERIA
-// AO IMPORTAR, DEVE PASSAR 3 PARAMETROS
-// PRIMEIRO: 0 - LOTOFACIL ou 1 - MEGA-SENA
-// SEGUNDO: QUANTIDADE DE JOGOS A SEREM GERADOS
-// TERCEIRO: QUANTIDADE DE NUMEROS QUE SERAO SORTEADOS
-
 const jogar = new loteria
 console.log(jogar.setGames(0, 10, 15))
 console.log(jogar.setGames(1, 5, 6))
